604398af15e759a50a797a142775473b2781c327
sagar <pawarsgr18@gmail.com>
2021-06-10 19:08:10 +0530

    Resolved Merge conflicts

--- src/Java/Basic/Programs/SwapString/StringSwapper.java
+++ src/Java/Basic/Programs/SwapString/StringSwapper.java
@@ -1,7 +1,7 @@
 package Java.Basic.Programs.SwapString;
 
 public class StringSwapper {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         String str1 = "SsAaGgRr";
         //str2
         String str2 = "Pawaroooooooooooooooooooooooooooooo";
@@ -12,5 +12,5 @@ public class StringSwapper {
         str2 = str2.substring(0,str2.length()-str1.length());
         System.out.println("S1: "+str1);
         System.out.println("S2: "+str2);
-    }
+    }*/
 }
